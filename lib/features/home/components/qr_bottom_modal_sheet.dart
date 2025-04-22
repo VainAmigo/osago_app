d4f2f78ff83e0df4eb07947234c2c9e230b0e701
VainAmigo <tonyginger15@gmail.com>
2025-04-22 16:04:59 +0600

    fixed qr page localization

---
+++ lib/features/home/components/qr_bottom_modal_sheet.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../../../common/components/qr_code_widget.dart';
+
+class QrCodeBottomModalSheet extends StatelessWidget {
+  final String polisId;
+
+  const QrCodeBottomModalSheet({super.key, required this.polisId});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(24),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      child: Column(
+        children: [
+          Text(
+            'No $polisId',
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.secondary,
+              fontSize: 14,
+            ),
+          ),
+          const SizedBox(height: 20),
+          QrCodeWidget(url: 'https://example.com')
+        ],
+      ),
+    );
+  }
+}
--- lib/features/home/osago_details_page.dart
+++ lib/features/home/osago_details_page.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:osago_bloc_app/common/components/my_button.dart';
+import 'package:osago_bloc_app/common/components/top_text_block.dart';
 import 'package:osago_bloc_app/save_and_open_pdf.dart';
 
 import '../../common/components/qr_code_widget.dart';
 import '../../common/localization/language_constants.dart';
 import '../osago/domain/entities/osago.dart';
 import '../osago/presentation/components/info_tile.dart';
+import 'components/qr_bottom_modal_sheet.dart';
 
 class OsagoDetailsPage extends StatefulWidget {
   final Osago osago;
@@ -25,72 +27,87 @@ class _OsagoDetailsPageState extends State<OsagoDetailsPage> {
 
   final PdfInvoiceService service = PdfInvoiceService();
 
+  // bottom sheet open button pressed
+  void _qrCodeButtonPressed() {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) => QrCodeBottomModalSheet(
+              polisId: widget.osago.id,
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Osago detail page'),
-        ),
+        appBar: AppBar(),
         body: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 24),
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Container(
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      color: Theme.of(context).colorScheme.inversePrimary,
-                    ),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        InfoTile(
-                            title: 'Полистин Ээси',
-                            content: widget.osago.polisOwnerName),
-                        InfoTile(
-                          title: 'Унаанын номери',
-                          content: widget.osago.carPlate,
-                        ),
-                        InfoTile(
-                          title: 'Башталыш жана Бутту',
-                          content:
-                              '${_dateTimeParser(widget.osago.startDate.toString())} - ${_dateTimeParser(widget.osago.endDate.toString())}',
-                        ),
-                        InfoTile(
-                          title: 'Баасы',
-                          content: '${widget.osago.costOfOsago}сом',
-                        ),
-                        InfoTile(
-                          title: 'Статусу',
-                          content: widget.osago.status ? 'Active' : 'Inactive',
-                        ),
-                      ],
-                    ),
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              children: [
+                topTextBlock(
+                  title: translation(context).osagoDetailPageTopTitle,
+                  text:
+                  translation(context).osagoDetailPageTopText,
+                ),
+                const SizedBox(height: 24),
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Theme.of(context).colorScheme.inversePrimary,
                   ),
-                  const SizedBox(height: 30),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      color: Theme.of(context).colorScheme.inversePrimary,
-                    ),
-                    child: QrCodeWidget(url: 'https://example.com'),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      InfoTile(
+                          title: translation(context).osagoDetailPageIngoTileOwner,
+                          content: widget.osago.polisOwnerName),
+                      InfoTile(
+                        title: translation(context).osagoDetailPageIngoTileCarPlate,
+                        content: widget.osago.carPlate,
+                      ),
+                      InfoTile(
+                        title: translation(context).osagoDetailPageIngoTileDates,
+                        content:
+                            '${_dateTimeParser(widget.osago.startDate.toString())} - ${_dateTimeParser(widget.osago.endDate.toString())}',
+                      ),
+                      InfoTile(
+                        title: translation(context).osagoDetailPageIngoTileCost,
+                        content: '${widget.osago.costOfOsago}сом',
+                      ),
+                      InfoTile(
+                        title: translation(context).osagoDetailPageIngoTileStatus,
+                        content: widget.osago.status ? translation(context).osagoDetailPageIngoTileStatusActive : translation(context).osagoDetailPageIngoTileStatusInactive,
+                      ),
+                    ],
                   ),
-                  const SizedBox(height: 30),
-                  MyButton(
-                    text: 'Создать PDF',
-                    onPress: () async {
-                      final data =
-                          await service.createOsagoDetailPdf(widget.osago);
-                      service.savePdfFile('pdf_file', data);
-                    },
+                ),
+                const Spacer(),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 40.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: MyButton(
+                          text: translation(context).buttonCreatePdf,
+                          onPress: () async {
+                            final data =
+                                await service.createOsagoDetailPdf(widget.osago);
+                            service.savePdfFile('pdf_file', data);
+                          },
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      IconButton(
+                          onPressed: () => _qrCodeButtonPressed(),
+                          icon: Icon(Icons.qr_code),
+                          color: Theme.of(context).primaryColor)
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ));
